1c672241a0f2d4c242d9c88834b1476d67058d85
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-07-02 13:23:17 +0700

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticPaths } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext, GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react'
 
@@ -15,14 +15,33 @@ const ProductDetail= ({}: ProdutProps) => {
   )
 }
 
-export const getStaticPaths: GetStaticPaths= async() =>{
-    const product = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
-    return {
-        props:{
-            
-        }
-    }
-}
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
+//     const paths = data.map((product: any) => (
+//       { params: { id: product.id } }
+//     ))
+//     return {
+//       paths,
+//       fallback: false 
+//     }
+//   }
+  
+//   export const getStaticProps: GetStaticProps<ProdutProps> = async (context: GetStaticPropsContext) => {
+//     console.log('context', context);
+//     const product = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params?.id}`)).json();
+//     return {
+//       props: {product},
+//       revalidate: 60
+//     }
+//   }
 
+
+export const getServerSideProps : GetServerSideProps= async (context: GetServerSidePropsContext) =>{
+  const product = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params?.id}`)).json();
+  return {
+    props: {product}
+    
+  }
+}
 
 export default ProductDetail
